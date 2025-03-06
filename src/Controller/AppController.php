09eecff0d5c97d11dab4e338e171e2d3b50f4ec7
Caroline MOREAU <karolynn75@hotmail.com>
2025-03-06 08:48:46 +0100

    first commit

---
+++ src/Controller/AppController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AppController extends AbstractController
+{
+    // page d'accueil
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('app/index.html.twig', [            
+        ]);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function appAbout(): Response
+    {
+        return $this->render('app/about.html.twig', [            
+        ]);
+    }
+
+
+       #[Route('/products', name: 'app_products')]
+    public function appProducts(): Response
+    {
+        return $this->render('app/products.html.twig', [            
+        ]);
+    }
+
+    #[Route('/whyus', name: 'app_whyus')]
+    public function appwhyus(): Response
+    {
+        return $this->render('app/whyus.html.twig', [            
+        ]);
+    }
+
+
+    #[Route('/testimonial', name: 'app_testimonial')]
+    public function appTestimonial(): Response
+    {
+        return $this->render('app/testimonial.html.twig', [            
+        ]);
+    }
+
+
+
+
+
+
+}
